2fc0126bf28bae80b88ec197cf5c6b3b824e671e
ManiArasuPandiyan <133253008+ManiArasuPandiyan@users.noreply.github.com>
2023-09-30 21:38:10 +0530

    Add files via upload

---
+++ ADSA/assignment1.py
@@ -0,0 +1,11 @@
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and key < a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+array = [5, 2, 9, 1, 5, 6]
+insertion_sort(array)
+print("Sorted Array:", array)
